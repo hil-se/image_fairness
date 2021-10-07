5dbb7d3a23f07d488d6f659cc9ecdc392c63d2ae
Zhe Yu <zhe@zhe-yus-mbp.wireless.rit.edu>
2021-10-07 17:14:24 -0400

    overfit

--- src/cnn.py
+++ src/cnn.py
@@ -5,16 +5,19 @@ class CNN:
     def __init__(self, start_size = 4):
         self.model = tf.keras.models.Sequential()
         self.model.add(
-            tf.keras.layers.Conv2D(start_size, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', input_shape=(200, 200, 3)))
+            tf.keras.layers.Conv2D(start_size, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', input_shape=(200, 200, 3), use_bias=True, kernel_regularizer =tf.keras.regularizers.l2( l=0.01)))
+        self.model.add(tf.keras.layers.Dropout(0.2))
         self.model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
         self.model.add(
-            tf.keras.layers.Conv2D(start_size*2, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+            tf.keras.layers.Conv2D(start_size*2, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', use_bias=True, kernel_regularizer =tf.keras.regularizers.l2( l=0.01)))
+        self.model.add(tf.keras.layers.Dropout( 0.2))
         self.model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
         self.model.add(
-            tf.keras.layers.Conv2D(start_size*4, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu'))
+            tf.keras.layers.Conv2D(start_size*4, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu', use_bias=True, kernel_regularizer =tf.keras.regularizers.l2( l=0.01)))
+        self.model.add(tf.keras.layers.Dropout(0.2))
         self.model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
         self.model.add(tf.keras.layers.Flatten())
-        self.model.add(tf.keras.layers.Dense(25*25*start_size*4, activation='relu'))
+        self.model.add(tf.keras.layers.Dense(25*25*start_size*4, activation='relu', use_bias=True, kernel_regularizer =tf.keras.regularizers.l2( l=0.01)))
         self.model.add(tf.keras.layers.Dense(2, activation='softmax'))
         self.model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'], optimizer='adam')
 
