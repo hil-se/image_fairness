c33a2d891164411e4dd762f1882cab1ce689e117
Zhe Yu <zhe@Zhe-Yus-MacBook-Pro.local>
2021-10-14 10:50:05 -0400

    slurm array

---
+++ src/nofair.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -l
+## NOTE the -l flag!
+
+## Name of the job -You'll probably want to customize this
+#SBATCH -J image_fairness_nofair
+
+## Use the resources available on this account
+#SBATCH -A loop
+
+## Standard out and Standard Error output files
+#SBATCH -o log/%J_%a.o
+#SBATCH -e log/%J_%a.e
+
+## To send mail for updates on the job
+## SBATCH --mail-user=zxyvse@rit.edu
+#SBATCH --mail-type=ALL
+
+## Request 3 Days, 5 Hours, 5 Minutes, 3 Seconds run time MAX,
+## anything over will be KILLED
+#SBATCH -t 3-03:05:03
+
+## Put in tier3 partition for testing small jobs, like this one
+## But because our requested time is over 4 day, it won't run, so
+## use any tier you have available
+#SBATCH -p tier3
+
+## Request 1 core for one task, note how you can put multiple commands
+## on one line
+#SBATCH -G 1
+
+## Job memory requirements in MB
+#SBATCH --mem=16G
+
+## Job script goes below this line
+
+spack unload -a
+## Load modules with spack
+## Tensorflow
+spack load /lklqe3u
+## matplotlib
+spack load /saj4vss
+## Execute target code
+Python3 main.py nofair ${SLURM_ARRAY_TASK_ID}
+
