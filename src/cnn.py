610aae4a16d4ac54c0f7a87dcde0d2261dc717d1
azhe825 <zyu9@ncsu.edu>
2021-11-14 13:53:56 -0500

    VGG16

--- src/cnn.py
+++ src/cnn.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 class VGG:
-    def __init__(self, start_size = 4):
+    def __init__(self, start_size = 16):
         self.model = tf.keras.models.Sequential()
         self.model.add(
             tf.keras.layers.Conv2D(start_size, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
@@ -68,33 +68,10 @@ class VGG:
 
     def predict(self, X):
         pred = self.model.predict(X)
-        # pred = np.array([np.argmax(row) for row in pred])
         pred = (pred.flatten()>0.5).astype(int).astype(float)
         return pred
 
     def load_model(self, checkpoint_filepath):
         self.model = tf.keras.models.load_model(checkpoint_filepath)
 
-class VGG16:
-    def __init__(self):
-        self.model = tf.keras.applications.VGG16(
-            include_top=True,
-            weights=None,
-            input_tensor=None,
-            input_shape=(200,200,3),
-            pooling=None,
-            classes=2,
-            classifier_activation="softmax",
-        )
-        self.model.compile(loss=tf.keras.losses.CategoricalCrossentropy(), metrics=['accuracy'], optimizer='adam')
-
-
-    def fit(self, X, y, sample_weight=None):
-        y_hot =tf.keras.utils.to_categorical(y, 2)
-        history = self.model.fit(X, y_hot, sample_weight=sample_weight, validation_split = 0.1, batch_size=128, epochs=10)
-        print(history.history)
-
-    def predict(self, X):
-        pred = self.model.predict(X)
-        return np.array([np.argmax(row) for row in pred])
 
