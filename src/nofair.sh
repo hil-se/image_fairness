dfd34494e32e72d8b2267c7993609dc44a502401
Zhe Yu <zyu9@ncsu.edu>
2021-11-15 10:06:51 -0500

    Update nofair.sh

--- src/nofair.sh
+++ src/nofair.sh
@@ -11,10 +11,6 @@
 #SBATCH -o log/%J_%a.o
 #SBATCH -e log/%J_%a.e
 
-## To send mail for updates on the job
-## SBATCH --mail-user=zxyvse@rit.edu
-#SBATCH --mail-type=ALL
-
 ## Request 3 Days, 5 Hours, 5 Minutes, 3 Seconds run time MAX,
 ## anything over will be KILLED
 #SBATCH -t 3-03:05:03
